d9b244ec701bff471b3dfeb0292cd5c046af11bd
kat2018 <kat.vge@gmail.com>
2018-06-22 12:45:21 -0400

    setup concurrently

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 //mogoose setup
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(process.nv.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
 
 const connection = mongoose.connection
 connection.on('connected', () => {
